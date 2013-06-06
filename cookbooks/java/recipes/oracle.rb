e9d8facda7153ed4c872c85e4a2d1e2e859a482e
Peter Donald <peter@realityforge.org>
2013-06-07 06:30:27 +1000

    Revert local change

--- cookbooks/java/recipes/oracle.rb
+++ cookbooks/java/recipes/oracle.rb
@@ -46,7 +46,7 @@ include_recipe "java::set_java_home"
 
 java_ark "jdk" do
   url tarball_url
-#  checksum tarball_checksum
+  checksum tarball_checksum
   app_home java_home
   bin_cmds bin_cmds
   alternatives_priority 1062
